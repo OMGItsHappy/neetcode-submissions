5b7b05333124e8a3f71e1e7d2c4277eb5d96ba90
Happy <61769826+OMGItsHappy@users.noreply.github.com>
2026-08-26 22:02:35 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        counts = defaultdict(int)
+        for i in range(len(s)):
+            counts[s[i]] += 1
+            counts[t[i]] -= 1
+        return not any(counts.values())
